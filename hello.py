d26f59756555d3df57d2f8b8d4102ccb3c9e7561
Pedro <user.name>
2018-10-14 02:56:24 -0700

    Añadida URL con parámetros

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 from flask import Flask
+try:
+    from PIL import Image
+except:
+    import Image
+from pintaMandelbrot import *
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,6 +19,19 @@ def usuaruioPepe():
 @app.route("/user/<usuario>")
 def todosLosUsuarios(usuario):
     return "Hola %s" %usuario
+
+@app.route('/pintaMandelbrot')
+def prog():   # los parámetros están en el request
+    parametro1 = request.args.get('x1')   # hola
+    parametro2 = request.args.get('y1')   # pepe
+    parametro1 = request.args.get('x2')   # hola
+    parametro2 = request.args.get('y2')   # pepe
+    pintaMandelbrot(x1, y1, x2, y2, 400, 255, "pintaMandelbrotPic.jpg");
+    response = Response()
+    response.headers.add('Content-Type', 'image/png')
+    imagen_jpg = Image.open("pintaMandelbrotPic.jpg");
+    response.set_data(imagen_jpg)
+    return response
 
 @app.errorhandler(404)
 def page_not_found(error):
