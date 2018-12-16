737ad015b005f4305b605f7551fce265ec443cd3
pedro <pedroluisfuertes@gmail.com>
2018-12-16 13:36:35 -0800

    Práctica7. jQuery y ajax

--- p6/sitio_web/restaurantes/urls.py
+++ p6/sitio_web/restaurantes/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
   	url(r'^test_template/$', views.test_template, name='test_template'),
   	url(r'^plato/add', views.add_plato, name='add_plato'),
+  	url(r'^lista_ajax', views.lista_ajax, name='lista_ajax'),
+  	url(r'^lista_jQuery', views.lista_jQuery, name='lista_jQuery'),
+  	url(r'^restajax/$', views.restajax, name='restajax'),
   	]
--- p6/sitio_web/restaurantes/views.py
+++ p6/sitio_web/restaurantes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from pymongo import MongoClient
 from .forms import *
 from .models import *
-
+import json
 # Create your views here.
 
 def index(request):
@@ -11,9 +11,80 @@ def index(request):
 def test_template(request):
     iterador = restaurantes.find().limit(10)
     context = {
-        "lista": list(iterador)
+      "lista": list(iterador)
     }
     return render(request, 'test.html', context)
+
+def lista_jQuery(request):
+    rest_total = 109
+    iterador = restaurantes.find().limit(rest_total)
+    rest_por_pag = 10
+    num_paginas = rest_total % rest_por_pag
+
+    if num_paginas == 0:
+        paginador = 0 
+    else:
+        paginador = range(0, int(rest_total / rest_por_pag + 1))
+
+    context = {
+        "lista": list(iterador),
+        "paginador": paginador,
+        "rest_por_pag": rest_por_pag
+    }
+    
+    return render(request, 'restaurantes_jQuery.html', context)
+
+def lista_ajax(request):
+
+    iterador = restaurantes.find().limit(10)
+
+    rest_por_pag = 10
+    num_paginas = iterador.count() % rest_por_pag
+
+    if num_paginas == 0:
+        paginador = range(1, int(iterador.count() / rest_por_pag + 1))
+    else:
+        paginador = range(1, int(iterador.count() / rest_por_pag + 2))
+
+
+    context = {
+        "lista": list(iterador),
+        "paginador": paginador
+    }
+    
+    return render(request, 'restaurantes_ajax.html', context)
+
+def restajax(request):
+  pag = int(request.GET.get('pagina',''))
+  pag = pag - 1
+
+  rest_por_pag = 10
+
+  rest = restaurantes.find().limit(10) 
+  
+  inicio = rest_por_pag * pag
+  fin = inicio
+  
+  rest_para_mostrar = int(rest.count() % rest_por_pag);
+
+  rest_aux = []
+
+  if pag == int(rest.count() / rest_por_pag):
+      fin += rest_para_mostrar
+      for i in range(0, rest_para_mostrar):
+          rest_aux.append([0])
+  else:
+      fin += rest_por_pag
+      for i in range(0,rest_por_pag):
+          rest_aux.append([None])
+          
+  i=0;
+  for item in rest[inicio:fin]:
+      rest_aux[i][0]=item['name']
+      i+=1;
+
+  return HttpResponse(json.dumps({'restaurantes': rest_aux}), content_type="application/json")
+
 
 def add_plato(request):
     if request.method == "POST":
